f23aa10adca0d2f74093a5eb5da1ae66a3dc576b
Sujal2006sharma <sujal2006sharma@gmail.com>
2025-05-04 11:34:02 +0530

    Create stringdelete.php

---
+++ stringdelete.php
@@ -0,0 +1,23 @@
+package ObjectOrientedProgramming;
+// Given:  ab3xympr2ktp
+// Output: abprp
+
+public class Stringg3delete
+{
+    public static void main(String[] args)
+    {
+        String str = "ab3xympr2ktp";
+        String g = "";
+        int i;
+        for (i = 0; i < str.length(); i++)
+        {
+            if (str.charAt(i) >= 48 && str.charAt(i) <= 57)
+            {
+                int num = str.charAt(i) - 48;
+                i = i + (num + 1);
+            }
+            g = g + str.charAt(i);
+        }
+        System.out.println(g);
+    }
+}
